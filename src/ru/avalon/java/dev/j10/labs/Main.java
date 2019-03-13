3cea94b5443644768b6121c1a2f6024a810b5e71
vshadrin <vshadrin1987@gmail.com>
2019-03-13 23:43:12 +0300

    Вроде разобрался с гитом. Файл называется lab1.2

--- src/ru/avalon/java/dev/j10/labs/Main.java
+++ src/ru/avalon/java/dev/j10/labs/Main.java
@@ -13,6 +13,7 @@ public class Main {
     
    public static void main(String[] args) {
 
+       
         /*
          * FIXME(Студент): Измените определение метода так, чтобы он стал точкой входа в приложение.
          */
